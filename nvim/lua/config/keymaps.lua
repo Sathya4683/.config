8e852814a4abbd5979b67ab37c28790bc46e10a7
Sathya Narayanan <sathyanarayanan2548@gmail.com>
2026-03-01 10:42:41 +0530

    update : dotfiles and created symlinks

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,49 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+----------------------------------------------------
+-- Better paste (do not overwrite clipboard)
+--------------------------------------------------
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+--------------------------------------------------
+-- Delete without yanking (black hole)
+--------------------------------------------------
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+--------------------------------------------------
+-- System clipboard yanks
+--------------------------------------------------
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+--------------------------------------------------
+-- Keep search results centered + open folds
+--------------------------------------------------
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+--------------------------------------------------
+-- Move selected lines up/down in visual mode
+--
+--------------------------------------------------
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+--------------------------------------------------
+-- Quickfix navigation (global list)
+--------------------------------------------------
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+
+--------------------------------------------------
+-- Location list navigation (window-local list)
+--------------------------------------------------
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set({ "n", "i" }, "<leader>w", "<cmd>w<CR>", { silent = true })
+vim.keymap.set("n", "<leader>q", "<cmd>close<CR>", { silent = true })
+vim.keymap.set("n", "<leader>qq", "<cmd>qa<CR>", { silent = true })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,4 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.relativenumber = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.scrolloff = 8
+vim.opt.undofile = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.clipboard = "unnamedplus"
